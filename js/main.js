f0418c1b46537c3293cd66aa1184c9bf64f605d3
toufiq23 <drrameraaron023@gmail.com>
2020-07-11 14:49:52 +0200

    iteration 1 done

--- js/main.js
+++ js/main.js
@@ -8,34 +8,47 @@ const W = ctx.canvas.width;
 const H = ctx.canvas.height;
 
 function draw() {
-  //
-  // Iteration 1: road drawing
-  //
+  
+	// Iteration 1: road drawing
+	const background = new Image();
+	background.src = "images/road.png";
 
-  // TODO
+// Make sure the image is loaded first otherwise nothing will draw.
+background.onload= function(){
+    ctx.drawImage(background,0, 0, W, H);   
+}
 
-  //
+  // const img = new Image();
+ 	// img.src = 'images/road.png';
+
+	//  let canvas, ctx, mainCanvas, mainCtx;
+
+	// img.onload = function() {
+  // // Create background canvas
+	// backgroundCanvas = document.getElementById('canvas');
+	
+	// const backgroundImage = {
+	// 	img: img,
+	// 	x: 0,
+	// 	y:0,
+	// 	speed: -1,
+	
+	// 	move: function() {
+	// 		this.y += this.speed;
+	// 		this.y %= canvas.height;
+	// 	},
+  
   // Iteration 2: car drawing
-  //
-
   // TODO
 
-  //
   // Iteration #4: obstacles
-  //
 
   // TODO
 
-  //
   // Iteration #5: collisions
-  //
-
   // TODO
 
-  //
   // Iteration #6: points
-  //
-
   // TODO
 
 }
